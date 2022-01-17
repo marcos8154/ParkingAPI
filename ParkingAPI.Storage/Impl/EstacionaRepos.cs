dd3e59fb84e4969e3880e87b2d0f2ef1aa762b22
Marcos Vinicius Pereira de Oliveira <marcos8154@gmail.com>
2022-01-17 07:33:15 -0300

    impl repos

---
+++ ParkingAPI.Storage/Impl/EstacionaRepos.cs
@@ -0,0 +1,63 @@
+﻿using Dapper;
+using IoCdotNet;
+using ParkingAPI.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingAPI.Storage.Impl
+{
+    internal sealed class EstacionaRepos : IEstacionamentoRepository
+    {
+        private readonly IDatabase db;
+        public EstacionaRepos()
+        {
+            db = IoC.Resolve<IDatabase>();
+        }
+
+        public Estacionamento ObterPorCnpj(string cnpj)
+        {
+            return db.Estacionamentos
+                .FirstOrDefault(e => e.CNPJ.Equals(cnpj));
+        }
+
+        public Estacionamento Find(object id)
+        {
+            return db.Estacionamentos.Find(id);
+        }
+
+        public void Add(Estacionamento e)
+        {
+            db.Estacionamentos.Add(e);
+            db.Commit();
+        }
+
+        public void Update(Estacionamento e)
+        {
+            db.Estacionamentos.Update(e);
+            db.Commit();
+        }
+
+        public void Remove(Estacionamento e)
+        {
+            db.Estacionamentos.Remove(e);
+            db.Commit();
+        }
+
+        public IReadOnlyCollection<Estacionamento> Where(Expression<Func<Estacionamento, bool>> query)
+        {
+            return db.Estacionamentos.Where(query)
+                .ToList();
+        }
+
+        public IReadOnlyCollection<Estacionamento> Where(string sql, object param)
+        {
+            using (IDbConnection conn = db.GetDbConnection())
+                return conn.Query<Estacionamento>(sql, param).ToList();
+        }
+    }
+}
---
+++ ParkingAPI.Storage/Impl/PlacaRepos.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using IoCdotNet;
+using ParkingAPI.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingAPI.Storage.Impl
+{
+    internal sealed class PlacaRepos : IPlacaRepository
+    {
+        private readonly IDatabase db;
+
+        public PlacaRepos()
+        {
+            db = IoC.Resolve<IDatabase>();
+        }
+
+        public Placa Find(object id)
+        {
+            return db.Placas.Find(id);
+        }
+
+        public void Add(Placa e)
+        {
+            db.Placas.Add(e);
+            db.Commit();
+        }
+
+        public void Update(Placa e)
+        {
+            db.Placas.Update(e);
+            db.Commit();
+        }
+
+        public void Remove(Placa e)
+        {
+            db.Placas.Remove(e);
+            db.Commit();
+        }
+
+        public IReadOnlyCollection<Placa> Where(Expression<Func<Placa, bool>> query)
+        {
+            return db.Placas.Where(query).ToList();
+        }
+
+        public IReadOnlyCollection<Placa> Where(string sql, object param)
+        {
+            using (IDbConnection conn = db.GetDbConnection())
+                return conn.Query<Placa>(sql, param).ToList();
+        }
+    }
+}
