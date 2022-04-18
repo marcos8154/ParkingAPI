e14581a4416f16912d15582a2494a7d1ade1cae5
laribarros <larissabarrose@gmail.com>
2022-04-18 19:28:18 -0300

    login usuario

--- ParkingAPI.Commands/Manipuladores/Usu/LogarUsuario.cs
+++ ParkingAPI.Commands/Manipuladores/Usu/LogarUsuario.cs
@@ -19,33 +19,13 @@ namespace ParkingAPI.Commands.Manipuladores.Usu
             usuarioRepos = IoC.Resolve<IUsuarioRepository>();
         }
 
-        public static string CalculaHash(string Senha)
-        {
-            try
-            {
-                System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
-                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(Senha);
-                byte[] hash = md5.ComputeHash(inputBytes);
-                System.Text.StringBuilder sb = new System.Text.StringBuilder();
-                for (int i = 0; i < hash.Length; i++)
-                {
-                    sb.Append(hash[i].ToString("X2"));
-                }
-                return sb.ToString();
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
         protected override ResultadoAcao ManipulaComando(LoginUsuario cmd)
         {
             try
             {
                 cmd.Valida();
 
-                Usuario usu = usuarioRepos.Login(cmd.Login, CalculaHash(cmd.Senha));
+                Usuario usu = usuarioRepos.Login(cmd.Login, cmd.Senha);
 
                 if (usu == null)
                     return new ResultadoAcao("Login e/ou senha incorretos.", StatusRetorno.NotFound);
